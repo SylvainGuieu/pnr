df442a09f8eee8db23a87b57ba0fc3a24bffe185
Sylvain Guieu <sylvain.guieu@gmail.com>
2016-11-22 11:06:18 -0300

    misc

--- __init__.py
+++ __init__.py
@@ -1,16 +1,30 @@
 from __future__ import print_function
 from vlt import *
-from dictionaries import loadDictionaries
-from processes import loadProcesses
+
+from .dictionaries import loadDictionaries
+from .processes import loadProcesses
+from .devices import loadDevices
 from .config import INSTRUMENT, say
+from vlt.config import config
+
+config['debug'] = True
+config['cdt']['path'] += ["/Users/sylvain/Dropbox/python/savevlt/CDT"]
+config['dictionary']['path'] += ["/Users/sylvain/Dropbox/python/savevlt/Dictionary", 
+								 "/Users/sylvain/Dropbox/python/savevlt/Dictionary/CCSLite/"]
+ 
 
 say(INSTRUMENT+" vlt interface\n")
-loadDictionaries(globals())
-say("")
-loadProcesses(globals())
-say("")
+glb = globals()
+#class toto:
+#	pass
+#glb = toto.__dict__
 
-
+loadDictionaries(glb)
+say("")
+loadProcesses(glb)
+say("")
+loadDevices(glb)
+pnoc.setDebug(True)
 
 
 
--- devices.py
+++ devices.py
@@ -1,24 +1,79 @@
 from __future__ import print_function
 from .config import INSTRUMENT, VERBOSE, say
+import vlt
 from vlt import devices
+
+
+def loadDevices(to):
+    loadMotors(to)
+    loadShutters(to)
+    loadDetector(to)
 
 def loadDetector(to):    
     pndict = to['pndict']
-    
+    pnoc = to['pnoc']
+
+    say("loadding Detector device ...")
     functions = pndict.restrict(
             [("DET.DIT","dit"),
              ("DET.NDIT","ndit"),
              ("DET.POLAR","polar"),
              ("DET.SUBWINS","subwins"),
-             ("DET.SUBWIN.COORINATES","subwincoord"),
+             ("DET.SUBWIN.COORDINATES", "subwincoord"),
              ("DET.SUBWINi.GEOMETRY","geometry")
             ]            
         ) 
     ## add some functions by hand
+    other_functions = vlt.FunctionDict({
+                          'type'   :vlt.Function( "DPR.TYPE", dtype=str , format="%s",
+                                                  value="DETCAR" ),
+                          'catg'   :vlt.Function( "DPR.CATG", dtype=str , format="%s",
+                                                  value="TEST" ),
+                          'tech'   :vlt.Function( "DPR.TECH", value="IMAGE",
+                                                  dtype=str , format="%s"),
+                          'imgname':vlt.Function( "OCS.DET.IMGNAME", value="PIONIER_{type}",
+                                                  dtype=str , format="%s"),
+                          'mode':   vlt.Function( "INS.MODE", dtype=str , format="%s",
+                                                  value="ENGINEERING")
 
+                          })
     
-    det = devices.Detector(
-        
-        
-        )
+    det = devices.Detector(functions+other_functions, proc=pnoc)
     
+    say("DET device added -> `det`")    
+    to['det'] = det
+
+def loadShutters(to):
+    ics = to['ics']
+    pnoc = to['pnoc']
+
+    say("loading shuter devices ...")
+
+    shutters = [devices.Shutter(ics.restrict("INS.SHUT%d"%(i)), statusItems=[""], proc=pnoc) for i in range(1,5)]
+    say("    INS.SHUT1 device ->  `shut1`")
+    say("    INS.SHUT2 device ->  `shut2`")
+    say("    INS.SHUT3 device ->  `shut3`")
+    say("    INS.SHUT4 device ->  `shut4`")
+
+    to['shut1'], to['shut2'], to['shut3'], to['shut4'] = shutters
+    say("  All Shuters -> `shutters`")
+    to['shutters'] = devices.Shutters(shutters)
+
+def loadMotors(to):
+    ics  = to['ics']
+    pnoc = to['pnoc']
+    
+    say("loading motorized devices ...")
+
+    say("    INS.OPTI1 device ->  `mirror`")
+    to['mirror'] = vlt.devices.Motor(ics.restrict("INS.OPTI1"), statusItems=["INS.OPTI1"], proc=pnoc)
+
+    say("    INS.OPTI2 device ->  `dycro`")
+    to['dycro']  = vlt.devices.Motor(ics.restrict("INS.OPTI2"), statusItems=["INS.OPTI2"], proc=pnoc)
+
+    say("    INS.OPTI3 device ->  `disp`")
+    to['disp']   = vlt.devices.Motor(ics.restrict("INS.OPTI3"), statusItems=["INS.OPTI3"], proc=pnoc)
+
+
+
+
